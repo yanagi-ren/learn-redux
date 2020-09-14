86f458bf499018b3d0af9a2ebe5a45cd369f0f34
Liu8 <renxiang@Liu8ui-Macmini.local>
2020-09-14 22:44:38 +0900

    Container Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import CounterContainer from './containers/CounterContainer';
+import TodosContainer from './containers/TodosContainer';
 
 function App() {
   return (
+    <div>
     <CounterContainer />
+    <hr />
+    <TodosContainer />
+    </div>
   );
 }
 
