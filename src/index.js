a7f0100f3492cb6281dd10a171044f935a175a6b
Liu8 <renxiang@Liu8ui-Macmini.local>
2020-09-14 22:01:36 +0900

    presentational components

--- src/index.js
+++ src/index.js
@@ -6,8 +6,9 @@ import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import rootReducer from './modules';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
   <Provider store={store}>
